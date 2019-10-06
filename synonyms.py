4427261069239755c8ccb3f88eeb8e0c314735e3
Friedrich Lindenberg <friedrich@pudo.org>
2019-10-06 17:05:20 +0200

    super raw sample file

--- synonyms.py
+++ synonyms.py
@@ -1,6 +1,6 @@
 import click
 from collections import defaultdict
-from Levenshtein import setmedian
+# from Levenshtein import setmedian
 from common import get_db
 
 # FORMAT
@@ -24,26 +24,31 @@ def export():
     mappings = defaultdict(set)
     for row in engine.query(QUERY):
         al = row.get('al')
-        an = row.get('an')
+        # an = row.get('an')
         bl = row.get('bl')
-        bn = row.get('bn')
+        # bn = row.get('bn')
         synonyms = al + bl
         unique = set(synonyms)
         canonical = max(unique, key=synonyms.count)
         # canonical = setmedian(synonyms)
         # unique.remove(canonical)
-        print(unique, canonical)
+        # print(unique, canonical)
         for name in unique:
+            if name == unique:
+                continue
             mappings[name].add(canonical)
-        
-        
+
         # num = row.get('num')
         # meta = '# %s <(%s)> %s\n' % (an, num, bn)
         # fh.write(meta)
         # synonyms = ' , '.join(unique)
         # fh.write('%s => %s\n\n' % (synonyms, canonical))
-    
+
     with open('synonyms.txt', 'w') as fh:
+        for name, expansions in mappings.items():
+            expansions = ' , '.join(expansions)
+            fh.write('%s => %s\n' % (name, expansions))
+            # print(name, expansions)
 
 
 if __name__ == '__main__':
